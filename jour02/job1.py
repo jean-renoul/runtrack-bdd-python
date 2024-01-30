0bae7ff2057f83f8865187a14a4c16473962ac7d
jean-renoul <jean.renoul@laplateforme.io>
2024-01-30 10:10:16 +0100

    job1 récupérer ensemble étudiants fait

---
+++ jour02/job1.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="patesaup0ulet",
+    database="laplateforme"
+)
+cursor = mydb.cursor()
+cursor.execute("SELECT * FROM etudiants")
+etudiants = cursor.fetchall()
+print (etudiants)
