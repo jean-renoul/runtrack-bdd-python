a1dbd0d2322dfe54ec682aea4b3b73bad81dd330
jean-renoul <jean.renoul@laplateforme.io>
2024-01-30 10:28:59 +0100

    ajouté cursor.close() + créé tables job2

--- jour02/job1.py
+++ jour02/job1.py
@@ -9,4 +9,5 @@ mydb = mysql.connector.connect(
 cursor = mydb.cursor()
 cursor.execute("SELECT * FROM etudiants")
 etudiants = cursor.fetchall()
+cursor.close()
 print (etudiants)
---
+++ jour02/job2.sql
@@ -0,0 +1,15 @@
+CREATE TABLE etage (
+id INT NOT NULL AUTO_INCREMENT,
+nom VARCHAR(255),
+numero INT,
+superficie INT,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE salle (
+id INT NOT NULL AUTO_INCREMENT,
+nom VARCHAR(255),
+id_etage INT,
+capacite INT,
+PRIMARY KEY (id)
+);
